b4efecd8c485953c72c8057c24fbc4e60d44b6c2
Jessica Atkinson <jesslynn1007@gmail.com>
2022-07-01 11:08:27 -0400

    message

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -8,6 +8,7 @@ import com.techelevator.services.CatPicService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 public class CatController {
@@ -28,15 +29,31 @@ public class CatController {
     }
 
     @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.GET)
-    public CatCard get(@PathVariable int id) {
+    public CatCard get(@PathVariable long id) {
         return catCardDao.get(id);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "/api/cards", method = RequestMethod.POST)
-    public boolean create(@RequestBody CatCard catCard) throws CatCardNotFoundException {
+    public boolean save(@RequestBody CatCard catCard) throws CatCardNotFoundException {
         return catCardDao.save(catCard);
     }
 
-    
+    @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.PUT)
+    public boolean update(@Valid @RequestBody CatCard catCard, @PathVariable long id) throws CatCardNotFoundException
+    {
+        return catCardDao.update(id,catCard);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable long id) throws CatCardNotFoundException
+    {
+        catCardDao.delete(id);
+    }
+
+    public CatCard randomCatCard()
+    {
+
+    }
 }
--- exercise/src/main/java/com/techelevator/services/RestCatPicService.java
+++ exercise/src/main/java/com/techelevator/services/RestCatPicService.java
@@ -3,14 +3,20 @@ package com.techelevator.services;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatPic;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 public class RestCatPicService implements CatPicService {
 
 	@Override
+	@RequestMapping(path = "https://cat-data.netlify.app/api/pictures/random", method = RequestMethod.GET)
 	public CatPic getPic() {
 		// TODO Auto-generated method stub
-		return null;
+		CatPic catPic = new CatPic();
+
+		catPic.setFile();
+		return getPic();
 	}
 
 }	
