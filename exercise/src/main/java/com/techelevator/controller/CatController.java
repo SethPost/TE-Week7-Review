415192fe2eb0f8097f582509be4bb852ae1f41ba
Seth Post <postsa@miamioh.edu>
2022-07-01 10:08:17 -0400

    Wrote list, get, and create methods in the CatController.java class

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -1,8 +1,14 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
+import com.techelevator.model.CatCardNotFoundException;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 public class CatController {
 
@@ -16,5 +22,21 @@ public class CatController {
         this.catPicService = catPicService;
     }
 
+    @RequestMapping(path = "/api/cards", method = RequestMethod.GET)
+    public List<CatCard> list() {
+        return catCardDao.list();
+    }
 
+    @RequestMapping(path = "/api/cards/{id}", method = RequestMethod.GET)
+    public CatCard get(@PathVariable int id) {
+        return catCardDao.get(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/api/cards", method = RequestMethod.POST)
+    public boolean create(@RequestBody CatCard catCard) throws CatCardNotFoundException {
+        return catCardDao.save(catCard);
+    }
+
+    
 }
